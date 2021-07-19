5f89bf7afd740617dc3e59f3998546f71200c76d
Matthew Sanchez <MSanchez@tql.com>
2021-07-19 16:31:55 -0400

    completed carrier detail, edit, list components

--- src/app/carrier/carrier-list/carrier-list.component.ts
+++ src/app/carrier/carrier-list/carrier-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Carrier } from '../carrier';
+import { CarrierService } from '../carrier.service';
 
 @Component({
   selector: 'app-carrier-list',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrierListComponent implements OnInit {
 
-  constructor() { }
+  carriers: Carrier [] = [];
+
+  searchCriteria: string = "";
+  sortColumn: string = "id";
+  sortAsc: boolean = true;
+  sortFn(column: string): void {
+    if (column === this.sortColumn){
+      this.sortAsc = !this.sortAsc;
+      return;
+    }
+    this.sortColumn = column;
+    this.sortAsc = true;
+  }
+
+  constructor(
+    private carriersvc: CarrierService
+  ) { }
 
   ngOnInit(): void {
+    this.carriersvc.list().subscribe(
+      res => {
+        console.log("Carriers:", res);
+        this.carriers = res;
+      },
+      err => {console.error(err);}
+    );
   }
 
 }
