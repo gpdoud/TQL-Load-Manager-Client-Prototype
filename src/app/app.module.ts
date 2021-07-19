a6da735fb908897b296d08d6a98ff5d8df1f7a81
Matthew Sanchez <MSanchez@tql.com>
2021-07-19 15:34:48 -0400

    created service, class, components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarrierListComponent } from './carrier/carrier-list/carrier-list.component';
+import { CarrierEditComponent } from './carrier/carrier-edit/carrier-edit.component';
+import { CarrierDetailComponent } from './carrier/carrier-detail/carrier-detail.component';
+import { CarrierCreateComponent } from './carrier/carrier-create/carrier-create.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarrierListComponent,
+    CarrierEditComponent,
+    CarrierDetailComponent,
+    CarrierCreateComponent,
+   
   ],
   imports: [
     BrowserModule,
