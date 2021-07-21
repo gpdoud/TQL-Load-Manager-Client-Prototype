e3ba03722cafd17ec98b8a9dc192b077be91ee2a
Matthew Sanchez <MSanchez@tql.com>
2021-07-21 10:03:48 -0400

    updated CarrierList added dispatch services

---
+++ src/app/carrier/dispatcher/dispatcher.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Dispatcher } from './dispatcher.class';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DispatcherService {
+  baseurl: string = "http://localhost:5000/api/dispatchers"
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  list(): Observable<Dispatcher[]> {
+    return this.http.get(`${this.baseurl}`) as Observable<Dispatcher[]>
+  }
+
+  get(id: number): Observable<Dispatcher> {
+    return this.http.get(`${this.baseurl}/${id}`) as Observable<Dispatcher>;
+  }
+
+  create(dispatcher: Dispatcher): Observable<Dispatcher> {
+    return this.http.post(`${this.baseurl}`, dispatcher) as Observable<Dispatcher>;
+  }
+
+  change(dispatcher: Dispatcher): Observable<any> {
+    return this.http.put(`${this.baseurl}/${dispatcher.id}`, dispatcher) as Observable<any>;
+  }
+
+  remove(id: number): Observable<Dispatcher> {
+    return this.http.delete(`${this.baseurl}/${id}`) as Observable<Dispatcher>;
+  }
+
+
+}
