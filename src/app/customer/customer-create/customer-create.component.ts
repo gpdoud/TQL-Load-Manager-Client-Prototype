1f9030b6c310cd469b3eb33a34b069412e928425
Katelyn Rains <KRains@tql.com>
2021-07-21 21:25:15 -0400

    finished customer component

--- src/app/customer/customer-create/customer-create.component.ts
+++ src/app/customer/customer-create/customer-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/user/user.class';
+import { UserService } from 'src/app/user/user.service';
+import { Customer } from '../customer.class';
+import { CustomerService } from '../customer.service';
 
 @Component({
   selector: 'app-customer-create',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-create.component.css']
 })
 export class CustomerCreateComponent implements OnInit {
+  customer: Customer = new Customer();
+  users: User[] = [];
 
-  constructor() { }
+  constructor(
+    private custsvc: CustomerService,
+    private usersvc: UserService,
+    private router: Router
+  ) { }
 
+  saveCreate(): void {
+    this.customer.userId =+ this.customer.userId;
+    this.custsvc.create(this.customer).subscribe(
+      res => {console.debug("edit successful");
+              this.router.navigateByUrl("/customer/list");},
+      err => {console.error(err);}        
+    );}
+  
   ngOnInit(): void {
+     this.usersvc.list().subscribe(
+        res => {this.users = res},
+        err => {console.error(err)}
+    )
   }
 
 }
